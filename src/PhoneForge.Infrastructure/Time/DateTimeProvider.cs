fb299876d877d7091391650b13c88e9bc52e276e
nwdorian <neuwirthdorian@gmail.com>
2025-11-15 10:58:13 +0100

    add XML comments

--- src/PhoneForge.Infrastructure/Time/DateTimeProvider.cs
+++ src/PhoneForge.Infrastructure/Time/DateTimeProvider.cs
@@ -2,7 +2,11 @@ using SharedKernel;
 
 namespace PhoneForge.Infrastructure.Time;
 
+/// <summary>
+/// Represents the machine date time service.
+/// </summary>
 internal sealed class DateTimeProvider : IDateTimeProvider
 {
+    /// <inheritdoc />
     public DateTime UtcNow => DateTime.UtcNow;
 }
--- src/PhoneForge.Persistence/PhoneForgeDbContext.cs
+++ src/PhoneForge.Persistence/PhoneForgeDbContext.cs
@@ -6,23 +6,34 @@ using SharedKernel;
 
 namespace PhoneForge.Persistence;
 
+/// <summary>
+/// Represents the applications database context.
+/// </summary>
 public sealed class PhoneForgeDbContext : DbContext, IDbContext
 {
     private readonly IDateTimeProvider _dateTimeProvider;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PhoneForgeDbContext"/> class.
+    /// </summary>
+    /// <param name="options">The database context options.</param>
+    /// <param name="dateTimeProvider">The current date and time in UTC format.</param>
     public PhoneForgeDbContext(DbContextOptions<PhoneForgeDbContext> options, IDateTimeProvider dateTimeProvider)
         : base(options)
     {
         _dateTimeProvider = dateTimeProvider;
     }
 
+    /// <inheritdoc />
     public DbSet<Contact> Contacts { get; set; }
 
+    /// <inheritdoc />
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(PhoneForgeDbContext).Assembly);
     }
 
+    /// <inheritdoc />
     public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
         var utcNow = _dateTimeProvider.UtcNow;
@@ -34,6 +45,10 @@ public sealed class PhoneForgeDbContext : DbContext, IDbContext
         return await base.SaveChangesAsync(cancellationToken);
     }
 
+    /// <summary>
+    /// Updates the entities implementing the <see cref="IAuditableEntity"/> interface.
+    /// </summary>
+    /// <param name="utcNow">The current date and time in UTC format.</param>
     private void UpdateAuditableEntities(DateTime utcNow)
     {
         var entities = ChangeTracker.Entries<IAuditableEntity>().ToList();
@@ -52,6 +67,10 @@ public sealed class PhoneForgeDbContext : DbContext, IDbContext
         }
     }
 
+    /// <summary>
+    /// Updates the entities implementing the <see cref="ISoftDeletableEntity"/> interface.
+    /// </summary>
+    /// <param name="utcNow">The current date and time in UTC format.</param>
     private void UpdateSoftDeletableEntities(DateTime utcNow)
     {
         var entities = ChangeTracker
@@ -74,6 +93,11 @@ public sealed class PhoneForgeDbContext : DbContext, IDbContext
         }
     }
 
+    /// <summary>
+    /// Updates the specified entity entry's referenced entries in the deleted state to the unchanged state.
+    /// This method is recursive.
+    /// </summary>
+    /// <param name="entity">The entity entry.</param>
     private static void UpdateDeletedEntityReferencesToUnchanged(EntityEntry entity)
     {
         var references = entity
