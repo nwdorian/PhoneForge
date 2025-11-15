f49ca2b9dc76e7736b9b81a1d9bfd5740839d21c
nwdorian <neuwirthdorian@gmail.com>
2025-11-15 15:24:26 +0100

    register DateTimeProvider with DI

---
+++ src/PhoneForge.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.DependencyInjection;
+using PhoneForge.Infrastructure.Time;
+using SharedKernel;
+
+namespace PhoneForge.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
+        return services;
+    }
+}
