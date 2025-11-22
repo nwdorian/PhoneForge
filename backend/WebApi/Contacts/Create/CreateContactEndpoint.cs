99689be9bb4ed9ba25e176f6b2c7738d413cf11b
nwdorian <neuwirthdorian@gmail.com>
2025-11-22 05:19:19 +0100

    change Create Contact endpoint to return 201 response and add endpoint metadata

--- backend/WebApi/Contacts/Create/CreateContactEndpoint.cs
+++ backend/WebApi/Contacts/Create/CreateContactEndpoint.cs
@@ -1,4 +1,6 @@
+using Application.Contacts;
 using Application.Contacts.Create;
+using WebApi.Contacts.GetById;
 using WebApi.Core;
 using WebApi.Core.Extensions;
 using WebApi.Core.Infrastructure;
@@ -13,6 +15,9 @@ internal sealed class CreateContactEndpoint : IEndpoint
     {
         app.MapPost(Routes.Contacts.Create, Handler)
             .WithNameAndTags(Name, Tags.Contacts)
+            .Produces<ContactResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status409Conflict)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
             .WithRequestValidation<CreateContactRequest>()
             .MapToApiVersion(1);
     }
@@ -32,6 +37,14 @@ internal sealed class CreateContactEndpoint : IEndpoint
 
         var result = await useCase.Handle(command, cancellationToken);
 
-        return result.Match(Results.Ok, CustomResults.Problem);
+        return result.Match(
+            result =>
+                Results.CreatedAtRoute(
+                    GetContactByIdEndpoint.Name,
+                    new { contactId = result.Id },
+                    result
+                ),
+            CustomResults.Problem
+        );
     }
 }
