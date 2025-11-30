a472524ef9302c34dc512ffba40d3c9a6a28bd58
nwdorian <neuwirthdorian@gmail.com>
2025-11-30 20:08:43 +0100

    add Page unit tests

---
+++ tests/UnitTests/Domain/Pagination/PageTests.cs
@@ -0,0 +1,31 @@
+using Domain.Core.Pagination;
+using Domain.Core.Primitives;
+
+namespace UnitTests.Domain.Pagination;
+
+public class PageTests
+{
+    [Fact]
+    public void Create_Should_ReturnSuccess_WithValidInput()
+    {
+        int input = 1;
+
+        Result<Page> result = Page.Create(input);
+
+        Assert.True(result.IsSuccess);
+        Assert.False(result.IsFailure);
+        Assert.Equal(input, result.Value);
+    }
+
+    [Fact]
+    public void Create_Should_ReturnError_WithTooLowInput()
+    {
+        int input = 0;
+
+        Result<Page> result = Page.Create(input);
+
+        Assert.True(result.IsFailure);
+        Assert.False(result.IsSuccess);
+        Assert.Equal(PaginationErrors.InvalidPage, result.Error);
+    }
+}
