b6a74325212fd7b79c765ccb9e9af1e7ed7f377f
nwdorian <neuwirthdorian@gmail.com>
2025-11-30 16:21:32 +0100

    add Contact unit tests

---
+++ tests/UnitTests/Contacts/CreateContact.cs
@@ -0,0 +1,26 @@
+using Domain.Contacts;
+
+namespace UnitTests.Contacts;
+
+public class CreateContact
+{
+    [Fact]
+    public void CreateContact_ShouldReturnContact_WithCorrectData()
+    {
+        FirstName firstName = FirstName.Create("John").Value;
+        LastName lastName = LastName.Create("Doe").Value;
+        Email email = Email.Create("jdoe@gmail.com").Value;
+        PhoneNumber phoneNumber = PhoneNumber.Create("0919876543").Value;
+        string fullName = $"{firstName.Value} {lastName.Value}";
+
+        Contact contact = Contact.Create(firstName, lastName, email, phoneNumber);
+
+        Assert.NotNull(contact);
+        Assert.NotEqual(Guid.Empty, contact.Id);
+        Assert.Equal(firstName.Value, contact.FirstName);
+        Assert.Equal(lastName.Value, contact.LastName);
+        Assert.Equal(email.Value, contact.Email);
+        Assert.Equal(phoneNumber.Value, contact.PhoneNumber);
+        Assert.Equal(fullName, contact.FullName);
+    }
+}
---
+++ tests/UnitTests/Contacts/UpdateContact.cs
@@ -0,0 +1,31 @@
+using Domain.Contacts;
+
+namespace UnitTests.Contacts;
+
+public class UpdateContact
+{
+    [Fact]
+    public void UpdateContact_ShouldChangeAllProperties()
+    {
+        FirstName firstName = FirstName.Create("John").Value;
+        LastName lastName = LastName.Create("Doe").Value;
+        Email email = Email.Create("jdoe@gmail.com").Value;
+        PhoneNumber phoneNumber = PhoneNumber.Create("0919876543").Value;
+
+        Contact contact = Contact.Create(firstName, lastName, email, phoneNumber);
+
+        FirstName newFirstName = FirstName.Create("Ada").Value;
+        LastName newLastName = LastName.Create("Lovelace").Value;
+        Email newEmail = Email.Create("alovelace@gmail.com").Value;
+        PhoneNumber newPhoneNumber = PhoneNumber.Create("0913456789").Value;
+        string newFullName = $"{newFirstName.Value} {newLastName.Value}";
+
+        contact.UpdateContact(newFirstName, newLastName, newEmail, newPhoneNumber);
+
+        Assert.Equal(newFirstName, contact.FirstName);
+        Assert.Equal(newLastName, contact.LastName);
+        Assert.Equal(newEmail, contact.Email);
+        Assert.Equal(newPhoneNumber, contact.PhoneNumber);
+        Assert.Equal(newFullName, contact.FullName);
+    }
+}
