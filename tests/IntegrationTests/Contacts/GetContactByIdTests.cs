04576b522dc63b7fd0ec4fac74bf2c107d2a20d9
nwdorian <neuwirthdorian@gmail.com>
2025-12-05 23:38:24 +0100

    add error assertions to GetContactyById test

--- tests/IntegrationTests/Contacts/GetContactByIdTests.cs
+++ tests/IntegrationTests/Contacts/GetContactByIdTests.cs
@@ -1,7 +1,11 @@
 using System.Net;
 using System.Net.Http.Json;
 using Application.Contacts;
+using Domain.Contacts;
+using Domain.Core.Primitives;
 using IntegrationTests.Core.Abstractions;
+using IntegrationTests.Core.Contracts;
+using IntegrationTests.Core.Extensions;
 using WebApi.Core.Constants;
 
 namespace IntegrationTests.Contacts;
@@ -27,11 +31,18 @@ public class GetContactByIdTests(IntegrationTestWebAppFactory factory)
     public async Task Should_ReturnNotFound_WhenContactDoesNotExist()
     {
         Guid contactId = Guid.NewGuid();
+        Error expected = ContactErrors.NotFoundById(contactId);
 
         HttpResponseMessage response = await HttpClient.GetAsync(
             $"{GetByIdRoute}/{contactId}"
         );
 
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+
+        CustomProblemDetails? problemDetails = await response.GetProblemDetails();
+
+        Assert.NotNull(problemDetails);
+        Assert.Equal(expected.Description, problemDetails.Errors[0]);
+        Assert.Equal(expected.Code, problemDetails.Title);
     }
 }
